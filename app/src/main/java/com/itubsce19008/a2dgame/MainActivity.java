e679311381138f29393b09a350110db67f6122a2
Atique Ahmad <bsce19008@itu.edu.pk>
2023-04-27 12:53:22 +0500

    first commit

--- app/src/main/java/com/itubsce19008/a2dgame/MainActivity.java
+++ app/src/main/java/com/itubsce19008/a2dgame/MainActivity.java
@@ -13,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
         MyCanvas c = new MyCanvas(getApplicationContext());
 
         setContentView(c);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        Thread.sleep(42);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    c.ballx = c.ballx + c.bdeltax;
+                    if(c.ballx+c.ballw/2 > c.width){
+                        c.bdeltax *= -1;
+                    }
+                    if (c.ballx<0)
+                    {
+                        c.bdeltax *= -1;
+                    }
+
+                    c.bally = c.bally + c.bdeltay;
+                    if(c.bally+c.ballh/2 > c.height){
+                        c.bdeltay *= -1;
+                    }
+                    if (c.bally+c.ballh<0)
+                    {
+                        c.bdeltay *= -1;
+                    }
+
+                    c.invalidate();
+                }
+            }
+        });
+        t.start();
     }
 }
--- app/src/main/java/com/itubsce19008/a2dgame/MyCanvas.java
+++ app/src/main/java/com/itubsce19008/a2dgame/MyCanvas.java
@@ -8,8 +8,10 @@ import android.view.View;
 
 public class MyCanvas extends View {
 
-    int ballx, bally, ballw, ballh;
+    int ballx, bally, ballw, ballh, bdeltax, bdeltay;
     int tabx, taby, tabw, tabh;
+
+    int width, height;
 
     public MyCanvas(Context context) {
         super(context);
@@ -17,6 +19,9 @@ public class MyCanvas extends View {
         ballx = bally =100;
         ballw =100;
         ballh =100;
+
+        bdeltax =5;
+        bdeltay =5;
 
         tabx =100;
         taby =100;
@@ -29,19 +34,23 @@ public class MyCanvas extends View {
         super.onDraw(canvas);
 
         canvas.drawColor(Color.WHITE);
-        int height = canvas.getHeight();
-        int width = canvas.getWidth();
+         height = canvas.getHeight();
+         width = canvas.getWidth();
 
         taby = height-200;
-        
+
         Paint red = new Paint();
         red.setColor(Color.RED);
 
         Paint blue = new Paint();
         blue.setColor(Color.BLUE);
 
+        Paint textpaint = new Paint();
+        textpaint.setColor(Color.CYAN);
+
         canvas.drawCircle(ballx,bally,ballw/2,red);
         canvas.drawRect(tabx,taby,tabx+tabw,taby+tabh,blue);
+        canvas.drawText("Hello: ",50, 50,textpaint);
 
     }
 }
