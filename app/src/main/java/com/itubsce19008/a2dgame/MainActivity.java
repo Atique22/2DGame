193757fecf77f7836e5f95f9f766ff1c4cd7aa59
Atique Ahmad <bsce19008@itu.edu.pk>
2023-04-27 13:26:01 +0500

    first commit

--- app/src/main/java/com/itubsce19008/a2dgame/MainActivity.java
+++ app/src/main/java/com/itubsce19008/a2dgame/MainActivity.java
@@ -23,27 +23,25 @@ public class MainActivity extends AppCompatActivity {
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
+
                     c.ballx = c.ballx + c.bdeltax;
-                    if(c.ballx+c.ballw/2 > c.width){
+                    if(c.ballx+c.ballw/2 > c.width)
                         c.bdeltax *= -1;
-                    }
+
                     if (c.ballx<0)
-                    {
                         c.bdeltax *= -1;
-                    }
+
 
                     c.bally = c.bally + c.bdeltay;
-//                    if(c.bally+c.ballh/2 > c.height){
-//                        c.bdeltay *= -1;
-//                    }
-                    if(c.bally> c.height){
+                    if(c.bally> c.height)
                         c.gameover=true;
-                    }else { c.gameover =false;}
+                    else
+                        c.gameover =false;
+
                     if (c.bally+c.ballh<0)
-                    {
                         c.bdeltay *= -1;
-                    }
-                    if(c.bally+c.ballh/2 >= c.taby && c.bally-c.ballh/2 <c.taby+c.tabh) {
+
+                    if(c.bally+c.ballh/2 > c.taby && c.bally-c.ballh/2 <c.taby+c.tabh) {
 //
                         if (c.ballx > c.tabx && c.ballx < (c.tabx + c.tabw)) {
                             c.bdeltay *= -1;
